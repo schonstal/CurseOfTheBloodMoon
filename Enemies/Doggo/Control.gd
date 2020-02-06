203def05b9bb2ebce5776c68f1f32ea87a8d99d9
Josh Schonstal <josh@schonstal.org>
2020-02-05 19:48:43 -0800

    🎮 Add basic dogs

--- Enemies/Doggo/Control.gd
+++ Enemies/Doggo/Control.gd
@@ -1,28 +1,34 @@
 extends Node2D
 
 export var speed = 1000
-export var attack_speed = 3000
 
 onready var parent = $'..'
 onready var detect_box = $DetectBox
 onready var animation = $'../Sprite/AnimationPlayer'
 onready var player = Game.scene.player
 
+var attacking = false
+
 var facing = LEFT setget set_facing,get_facing
 
 const RIGHT = -1
 const LEFT = 1
 
+func _ready():
+  animation.connect("animation_finished", self, "_on_Animation_finished")
+
 func _process(delta):
-  if Game.scene != null && player != null && is_instance_valid(player):
+  if Game.scene != null && player != null && is_instance_valid(player) && !attacking:
     if detect_box.overlaps_body(player):
+      attacking = true
       animation.play("Attack")
       parent.acceleration = Vector2.ZERO
       parent.velocity = Vector2.ZERO
     else:
+      animation.play("Run")
       var direction = Game.scene.player.global_position - global_position
-      parent.acceleration = attack_speed * direction.normalized()
-      if player.position.x < position.x:
+      parent.acceleration = speed * direction.normalized()
+      if player.global_position.x < global_position.x:
         self.facing = LEFT
       else:
         self.facing = RIGHT
@@ -36,3 +42,7 @@ func set_facing(value):
 
 func get_facing():
   return facing
+
+func _on_Animation_finished(name):
+  if name == "Attack":
+    attacking = false
