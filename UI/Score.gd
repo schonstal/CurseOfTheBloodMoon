a41278906e6a74efadbf7c900f27aa8f7fa9164f
Josh Schonstal <josh@schonstal.org>
2020-02-07 16:08:08 -0800

    ✨ Update HUD score text

--- UI/Score.gd
+++ UI/Score.gd
@@ -1,4 +1,13 @@
 extends Label
 
+var previous_score = 0
+
+onready var animation = $AnimationPlayer
+
 func _process(_delta):
   self.text = "%d" % Game.scene.score
+  
+  if Game.scene.score > previous_score:
+    animation.play("EarnedPoints")
+    
+  previous_score = Game.scene.score
