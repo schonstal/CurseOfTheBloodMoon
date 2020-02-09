48abad90cd70c73f380d29c851d1ab06ff37c24b
Josh Schonstal <josh@schonstal.org>
2020-02-09 09:50:29 -0800

    ✨ Strengthen screen shake on player damage

--- Player/Player.gd
+++ Player/Player.gd
@@ -92,7 +92,7 @@ func hurt(damage):
     die()
 
   EventBus.emit_signal("player_hurt", health)
-  EventBus.emit_signal("shake_camera", 0.25, 60, 5)
+  EventBus.emit_signal("shake_camera", 0.25, 60, 20)
   Overlay.fade(Color(0.733, 0.145, 0.192, 0.4), Color(0.733, 0.145, 0.192, 0), 0.3)
   EventBus.emit_signal("reset_combo")
   Game.scene.sound.play(hurt_sound, "player_hurt")
