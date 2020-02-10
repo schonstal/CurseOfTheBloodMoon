92860feded677721ae7227132d41555ff93b23cb
Josh Schonstal <josh@schonstal.org>
2020-02-10 13:17:54 -0800

    ✨ Add details to transition

--- UI/Transition/Transition.gd
+++ UI/Transition/Transition.gd
@@ -1,18 +1,33 @@
 extends CanvasLayer
 
+export var shake_duration = 0.25
+export var tween_in_duration = 0.25
+export var tween_out_duration = 0.25
+
 onready var tween = $Tween
 onready var left = $Left
 onready var right = $Right
+onready var start_sound = $StartSound
+onready var end_sound = $EndSound
 
 var width = 0
 var shake_time = 0
+var vp_width = 1920
+
+var left_origin
+var right_origin
 
 signal transition_complete
 
 func _ready():
   width = left.texture.get_width()
-  left.position.x = -width
-  right.position.x = 1920
+  left.position.x = -width - 150
+  right.position.x = vp_width + 150
+  left.visible = true
+  right.visible = true
+
+  left_origin = left.position.x
+  right_origin = right.position.x
 
   tween.connect("tween_completed", self, "_on_Tween_completed")
 
@@ -25,46 +40,46 @@ func _process(delta):
     offset = Vector2.ZERO
 
 func transition_out():
-  var vp_width = 1920
+  start_sound.play()
 
   tween.interpolate_property(
       left,
       "position",
-      Vector2(-width, 0),
-      Vector2(vp_width / 2 - width, 0),
-      0.5,
+      Vector2(left_origin, -25),
+      Vector2(vp_width / 2 - width, -25),
+      tween_in_duration,
       Tween.TRANS_QUART,
       Tween.EASE_IN)
 
   tween.interpolate_property(
       right,
       "position",
-      Vector2(vp_width, 0),
-      Vector2(vp_width / 2, 0),
-      0.5,
+      Vector2(right_origin, -25),
+      Vector2(vp_width / 2, -25),
+      tween_in_duration,
       Tween.TRANS_QUART,
       Tween.EASE_IN)
 
   tween.start()
 
 func transition_in():
-  var vp_width = 1920
+  start_sound.play()
 
   tween.interpolate_property(
       left,
       "position",
-      Vector2(vp_width / 2 - width, 0),
-      Vector2(-width, 0),
-      0.5,
+      Vector2(vp_width / 2 - width, -25),
+      Vector2(left_origin, -25),
+      tween_out_duration,
       Tween.TRANS_QUART,
       Tween.EASE_IN)
 
   tween.interpolate_property(
       right,
       "position",
-      Vector2(vp_width / 2, 0),
-      Vector2(vp_width, 0),
-      0.5,
+      Vector2(vp_width / 2, -25),
+      Vector2(right_origin, -25),
+      tween_out_duration,
       Tween.TRANS_QUART,
       Tween.EASE_IN)
 
@@ -74,4 +89,5 @@ func _on_Tween_completed(_object, _key):
   emit_signal("transition_complete")
 
   if right.position.x < 1000:
-    shake_time = 0.5
+    shake_time = shake_duration
+    end_sound.play()
