4761f9a3d891b108d16c1a9e5720df8fb8d4a3e2
Josh Schonstal <josh@schonstal.org>
2020-02-14 10:14:53 -0800

    🎮 Add basic fly spawning

--- Scenes/Spawn.gd
+++ Scenes/Spawn.gd
@@ -3,20 +3,38 @@ extends YSort
 onready var bat_spawn_timer = $BatSpawnTimer
 onready var turret_spawn_timer = $TurretSpawnTimer
 onready var dog_spawn_timer = $DogSpawnTimer
+onready var fly_spawn_timer = $FlySpawnTimer
 
 var spawn_scene = preload("res://Enemies/EnemySpawn/Spawner.tscn")
 var bat_scene = preload("res://Enemies/Bat/Bat.tscn")
 var turret_scene = preload("res://Enemies/Turret/Turret.tscn")
 var dog_scene = preload("res://Enemies/Doggo/Doggo.tscn")
+var fly_scene = preload("res://Enemies/Fly/Fly.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
   bat_spawn_timer.connect("timeout", self, "_on_BatSpawnTimer_timeout")
   turret_spawn_timer.connect("timeout", self, "_on_TurretSpawnTimer_timeout")
   dog_spawn_timer.connect("timeout", self, "_on_DogSpawnTimer_timeout")
+  fly_spawn_timer.connect("timeout", self, "_on_FlySpawnTimer_timeout")
+
+func _on_FlySpawnTimer_timeout():
+  fly_spawn_timer.wait_time = max(5.0, 10.0 - (10 * Game.scene.score / 100000.0))
+
+  for index in range(0, 8):
+    var fly = spawn_scene.instance()
+    var angle = TAU * index / 8
+    var radius = 500
+
+    fly.global_position = Game.scene.player.global_position + \
+        Vector2(cos(angle), sin(angle)) * \
+        radius
+
+    fly.connect("spawn", self, "spawn_fly")
+    Game.scene.bodies.call_deferred("add_child", fly)
 
 func _on_BatSpawnTimer_timeout():
-  bat_spawn_timer.wait_time = max(1.0, 2.0 - (2 * Game.scene.score / 100000.0))
+  bat_spawn_timer.wait_time = max(2.0, 4.0 - (4 * Game.scene.score / 100000.0))
 
   var spawn = spawn_scene.instance()
   spawn.global_position = Vector2(
@@ -27,7 +45,7 @@ func _on_BatSpawnTimer_timeout():
   spawn.connect("spawn", self, "spawn_bat")
 
 func _on_TurretSpawnTimer_timeout():
-  turret_spawn_timer.wait_time = max(6.0, 15.0 - (15 * Game.scene.score / 100000.0))
+  turret_spawn_timer.wait_time = max(8.0, 15.0 - (15 * Game.scene.score / 100000.0))
 
   var spawn = spawn_scene.instance()
   spawn.global_position = Vector2(
@@ -60,5 +78,10 @@ func spawn_turret(position):
 
 func spawn_dog(position):
   var spawn = dog_scene.instance()
+  spawn.global_position = position
+  Game.scene.bodies.call_deferred("add_child", spawn)
+
+func spawn_fly(position):
+  var spawn = fly_scene.instance()
   spawn.global_position = position
   Game.scene.bodies.call_deferred("add_child", spawn)
