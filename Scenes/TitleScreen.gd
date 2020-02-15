c542d1c2819432ff55a728d35e22b23d09e7ea69
Josh Schonstal <josh@schonstal.org>
2020-02-15 15:10:25 -0800

    🐛 Fix bug where transition could be triggered multiple times

--- Scenes/TitleScreen.gd
+++ Scenes/TitleScreen.gd
@@ -2,10 +2,13 @@ extends Control
 
 onready var animation = $VBoxContainer/Label/AnimationPlayer
 
+var started = false
+
 func _ready():
   MusicPlayer.play_file("res://Music/menu.ogg")
-  
+
 func _process(delta):
-  if Input.is_action_just_pressed("start"):
+  if !started && Input.is_action_just_pressed("start"):
+    started = true
     animation.play("Flicker")
     Game.reset()
--- UI/GameOver/GameOver.gd
+++ UI/GameOver/GameOver.gd
@@ -1,5 +1,8 @@
 extends Control
 
+var started = false
+
 func _process(delta):
-  if Input.is_action_just_pressed("melee") || Input.is_action_just_pressed("shoot"):
+  if !started && (Input.is_action_just_pressed("melee") || Input.is_action_just_pressed("shoot")):
+    started = true
     Game.reset()
