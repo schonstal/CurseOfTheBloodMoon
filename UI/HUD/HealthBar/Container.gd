a259e96044821c9d8a5fe57d6567fd30a38b1b4b
Josh Schonstal <josh@schonstal.org>
2020-02-19 10:53:48 -0800

    🔧 Update container scaling to match new assets

--- UI/HUD/HealthBar/Container.gd
+++ UI/HUD/HealthBar/Container.gd
@@ -7,10 +7,14 @@ onready var right = $Right
 onready var middle = $Middle
 
 var length_was = 0.0
+var height = 65
+
+func _ready():
+  height = middle.region_rect.size.y
 
 func _process(_delta):
   if length != length_was:
-    middle.region_rect = Rect2(0, 0, length, 30)
-    right.position.x = length + 119
+    middle.region_rect = Rect2(0, 0, length, height)
+    right.position.x = length + 281
 
     length_was = length
