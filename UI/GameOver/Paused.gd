bac30b37dd9b6423a3a28869992b8a11b9f5d217
Josh Schonstal <josh@schonstal.org>
2020-02-20 19:08:52 -0800

    🐛 Fix bug where you could pause on game over screen

--- UI/GameOver/Paused.gd
+++ UI/GameOver/Paused.gd
@@ -2,8 +2,13 @@ extends Control
 
 func _ready():
   pause_mode = Node.PAUSE_MODE_PROCESS
-  
+
 func _process(delta):
+  if Game.scene.is_game_over:
+    get_tree().paused = false
+    visible = false
+    return
+
   visible = get_tree().paused
-  if Input.is_action_just_pressed("start"):
+  if Input.is_action_just_pressed("pause"):
     get_tree().paused = !get_tree().paused
